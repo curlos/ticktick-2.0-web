9f74e6cd3addfbde44ba225addd139841b111c2b
Carlos Martinez <curlosmart@gmail.com>
2024-06-12 06:31:42 -0400

    Add task list to Eisenhower Matrix

--- src/components/EisenhowerMatrix.tsx
+++ src/components/EisenhowerMatrix.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useGetProjectsQuery, useGetTasksQuery } from '../services/api';
 import { getTasksWithNoParent } from '../utils/helpers.utils';
 import { SMART_LISTS } from '../utils/smartLists.utils';
+import TaskListByCategory from './TaskListByCategory';
+import Icon from './Icon';
 
 const EisenhowerMatrix = () => {
 	const { data: fetchedProjects, isLoading: isLoadingProjects, error: errorProjects } = useGetProjectsQuery();
@@ -29,7 +31,7 @@ const EisenhowerMatrix = () => {
 		<div className="w-full h-full max-h-screen bg-color-gray-700 p-4 flex flex-col">
 			<h1 className="font-medium text-[20px]">Eisenhower Matrix</h1>
 
-			<div className="flex-1 grid grid-cols-2 gap-2">
+			<div className="flex-1 max-h-[90vh] grid grid-cols-2 gap-2">
 				<MatrixSquare tasksWithNoParent={tasksWithNoParent} priority={3} />
 				<MatrixSquare tasksWithNoParent={tasksWithNoParent} priority={3} />
 				<MatrixSquare tasksWithNoParent={tasksWithNoParent} priority={3} />
@@ -40,12 +42,39 @@ const EisenhowerMatrix = () => {
 };
 
 const MatrixSquare = ({ tasksWithNoParent, priority }) => {
-	return (
-		<div className="w-full rounded-lg bg-color-gray-600 p-3">
-			<div>Urgent & Important</div>
+	const iconClass = '!text-[20px] p-[3px] rounded hover:bg-color-gray-200';
 
-			{/* TODO: Map tasks */}
-			{/* <div>{tasksWithNoParent}</div> */}
+	return (
+		<div className="w-full rounded-lg bg-color-gray-600 p-3 h-full">
+			<div className="mb-3 flex justify-between items-center cursor-pointer group">
+				<div>Urgent & Important</div>
+
+				{/* <div className="invisible group-hover:visible">
+					<Icon name="add" fill={0} customClass={'!text-[20px]'} />
+					<Icon name="more_horiz" fill={0} customClass={'!text-[20px] ml-2'} />
+				</div> */}
+
+				<div className="text-color-gray-100 space-x-1">
+					<Icon name="add" fill={0} customClass={iconClass} />
+					<Icon name="more_horiz" fill={0} customClass={iconClass} />
+				</div>
+			</div>
+
+			<div className="overflow-auto gray-scrollbar max-h-[39vh]">
+				<TaskListByCategory
+					tasks={tasksWithNoParent.filter((task) => {
+						if (task.isDeleted) {
+							return false;
+						}
+
+						if (task.willNotDo) {
+							return false;
+						}
+
+						return true;
+					})}
+				/>
+			</div>
 		</div>
 	);
 };
