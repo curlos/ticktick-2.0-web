e7710ca8e6840df62898c793e81b1e7e543e42cb
Carlos Martinez <curlosmart@gmail.com>
2024-06-17 07:27:43 -0400

    Refactored DropdownDates into a separate file + properly send the dateOptions information to the backend when editing the matrix.

---
+++ src/components/Dropdown/DropdownDates.tsx
@@ -0,0 +1,189 @@
+import classNames from 'classnames';
+import { useState, useRef, useEffect } from 'react';
+import { DropdownProps } from '../../interfaces/interfaces';
+import Icon from '../Icon';
+import Dropdown from './Dropdown';
+import DropdownCalendar from './DropdownCalendar/DropdownCalendar';
+
+interface DropdownDatesProps extends DropdownProps {
+	selectedDate: string;
+	setSelectedDate: string;
+}
+
+const DropdownDates: React.FC<DropdownDatesProps> = ({
+	toggleRef,
+	isVisible,
+	setIsVisible,
+	dateOptions,
+	setDateOptions,
+}) => {
+	const [localDateOptions, setLocalDateOptions] = useState(dateOptions);
+
+	const [fromDate, setFromDate] = useState(null);
+	const [toDate, setToDate] = useState(null);
+	const [isDropdownFromDateVisible, setIsDropdownFromDateVisible] = useState(false);
+	const [isDropdownToDateVisible, setIsDropdownToDateVisible] = useState(false);
+
+	const dropdownFromDateRef = useRef();
+	const dropdownToDateRef = useRef();
+
+	useEffect(() => {
+		setLocalDateOptions(dateOptions);
+
+		const { duration } = dateOptions;
+
+		// TODO: Time doesn't seem to be saving, only the date. Fix that.
+		setFromDate(new Date(duration.fromDate));
+		setToDate(new Date(duration.toDate));
+	}, [dateOptions]);
+
+	const Duration = () => {
+		return (
+			<div className="flex-1 space-y-2 w-full">
+				<DurationOption
+					dropdownDateRef={dropdownFromDateRef}
+					isDropdownDateVisible={isDropdownFromDateVisible}
+					setIsDropdownDateVisible={setIsDropdownFromDateVisible}
+					date={fromDate}
+					setDate={setFromDate}
+					dropdownCustomClasses=" !ml-[100px] mt-[-100px]"
+				/>
+
+				<DurationOption
+					dropdownDateRef={dropdownToDateRef}
+					isDropdownDateVisible={isDropdownToDateVisible}
+					setIsDropdownDateVisible={setIsDropdownToDateVisible}
+					date={toDate}
+					setDate={setToDate}
+					dropdownCustomClasses=" !ml-[100px] mt-[-60px]"
+				/>
+			</div>
+		);
+	};
+
+	const DurationOption = ({
+		dropdownDateRef,
+		isDropdownDateVisible,
+		setIsDropdownDateVisible,
+		date,
+		setDate,
+		dropdownCustomClasses,
+	}) => (
+		<div className="flex-1 w-full" onClick={(e) => e.stopPropagation()}>
+			<div
+				ref={dropdownDateRef}
+				className="border border-color-gray-200 rounded p-1 px-2 flex justify-between items-center hover:border-blue-500 rounded-md cursor-pointer"
+				onClick={(e) => {
+					e.stopPropagation();
+					setIsDropdownDateVisible(!isDropdownDateVisible);
+				}}
+			>
+				<div className="max-w-[300px] truncate text-[12px]">
+					{date
+						? date.toLocaleString('en-US', {
+								year: 'numeric', // Full year
+								month: 'long', // Full month name
+								day: 'numeric', // Day of the month
+								hour: 'numeric', // Hour (in 12-hour AM/PM format)
+								minute: '2-digit', // Minute with leading zeros
+								hour12: true, // Use AM/PM
+							})
+						: 'No Date'}
+				</div>
+				<Icon
+					name="expand_more"
+					fill={0}
+					customClass={'text-color-gray-50 !text-[18px] hover:text-white cursor-pointer'}
+				/>
+			</div>
+
+			<DropdownCalendar
+				toggleRef={dropdownDateRef}
+				isVisible={isDropdownDateVisible}
+				setIsVisible={setIsDropdownDateVisible}
+				currDueDate={date}
+				setCurrDueDate={setDate}
+				customClasses={dropdownCustomClasses}
+			/>
+		</div>
+	);
+
+	return (
+		<Dropdown
+			toggleRef={toggleRef}
+			isVisible={isVisible}
+			setIsVisible={setIsVisible}
+			customClasses={'mt-[5px] shadow-2xl border border-color-gray-200 rounded-lg w-full'}
+		>
+			<div className="rounded" onClick={(e) => e.stopPropagation()}>
+				<div
+					className={classNames(
+						'p-1 gray-scrollbar max-h-[200px]',
+						!isDropdownFromDateVisible && !isDropdownToDateVisible ? 'overflow-auto' : 'overflow-hidden'
+					)}
+				>
+					{localDateOptions &&
+						Object.entries(localDateOptions).map(([key, value]) => {
+							const isDateSelected = value.selected;
+
+							return (
+								<div
+									key={value.name}
+									className="flex items-center justify-between gap-2 hover:bg-color-gray-300 p-2 rounded-lg cursor-pointer"
+									onClick={() => {
+										setLocalDateOptions({
+											...localDateOptions,
+											[key]: { ...value, selected: !localDateOptions[key].selected },
+										});
+									}}
+								>
+									<div className="flex-1 flex items-center gap-2">
+										<Icon
+											name={value.iconName}
+											fill={0}
+											customClass={classNames(
+												'!text-[18px] hover:text-white cursor-pointer',
+												isDateSelected ? 'text-blue-500' : ''
+											)}
+										/>
+										<div className={isDateSelected ? 'text-blue-500' : ''}>{value.name}</div>
+										{key === 'duration' && <Duration />}
+									</div>
+									{isDateSelected && (
+										<Icon
+											name="check"
+											fill={0}
+											customClass={'text-blue-500 !text-[18px] hover:text-white cursor-pointer'}
+										/>
+									)}
+								</div>
+							);
+						})}
+				</div>
+
+				<div className="p-3 border-t border-color-gray-200 flex gap-2">
+					<button
+						className="flex-1 border border-color-gray-200 rounded py-1 cursor-pointer hover:bg-color-gray-200 min-w-[114px]"
+						onClick={() => setIsVisible(false)}
+					>
+						Cancel
+					</button>
+					<button
+						className="flex-1 bg-blue-500 rounded py-1 cursor-pointer hover:bg-blue-600 min-w-[114px]"
+						onClick={() => {
+							setDateOptions({
+								...localDateOptions,
+								duration: { ...localDateOptions['duration'], fromDate: fromDate, toDate: toDate },
+							});
+							setIsVisible(false);
+						}}
+					>
+						Ok
+					</button>
+				</div>
+			</div>
+		</Dropdown>
+	);
+};
+
+export default DropdownDates;
--- src/components/Modal/ModalEditMatrix.tsx
+++ src/components/Modal/ModalEditMatrix.tsx
@@ -5,73 +5,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setModalState } from '../../slices/modalSlice';
 import CustomInput from '../CustomInput';
 import { useEffect, useRef, useState } from 'react';
-import { DropdownProps } from '../../interfaces/interfaces';
-import Dropdown from '../Dropdown/Dropdown';
 import { useEditMatrixMutation, useGetProjectsQuery } from '../../services/api';
 import DropdownProjects from '../Dropdown/DropdownProjects';
 import { SMART_LISTS } from '../../utils/smartLists.utils';
 import CustomRadioButton from '../CustomRadioButton';
-import DropdownCalendar from '../Dropdown/DropdownCalendar/DropdownCalendar';
-
-const DEFAULT_ALL_DATES = {
-	all: {
-		name: 'All',
-		iconName: 'stacks',
-		selected: true,
-	},
-	'no date': {
-		name: 'No Date',
-		iconName: 'hourglass_empty',
-		selected: false,
-	},
-	overdue: {
-		name: 'Overdue',
-		iconName: 'west',
-		selected: false,
-	},
-	repeat: {
-		name: 'Repeat',
-		iconName: 'repeat',
-		selected: false,
-	},
-	today: {
-		name: 'Today',
-		iconName: 'calendar_today',
-		selected: false,
-	},
-	tomorrow: {
-		name: 'Tomorrow',
-		iconName: 'upcoming',
-		selected: false,
-	},
-	'this week': {
-		name: 'This Week',
-		iconName: 'event_upcoming',
-		selected: false,
-	},
-	'next week': {
-		name: 'Next Week',
-		iconName: 'stacks',
-		selected: false,
-	},
-	'this month': {
-		name: 'This Month',
-		iconName: 'dark_mode',
-		selected: false,
-	},
-	'next month': {
-		name: 'Next Month',
-		iconName: 'nights_stay',
-		selected: false,
-	},
-	duration: {
-		name: 'Duration',
-		iconName: 'timer',
-		selected: false,
-		from: null,
-		to: null,
-	},
-};
+import DropdownDates from '../Dropdown/DropdownDates';
 
 const ModalEditMatrix: React.FC = () => {
 	const modal = useSelector((state) => state.modals.modals['ModalEditMatrix']);
@@ -108,7 +46,7 @@ const ModalEditMatrix: React.FC = () => {
 		low: matrix?.selectedPriorities?.low,
 		none: matrix?.selectedPriorities?.none,
 	});
-	const [allDates, setAllDates] = useState(DEFAULT_ALL_DATES);
+	const [dateOptions, setDateOptions] = useState(matrix?.dateOptions);
 
 	if (!modal) {
 		return null;
@@ -119,10 +57,10 @@ const ModalEditMatrix: React.FC = () => {
 	useEffect(() => {
 		if (matrix) {
 			// TODO: FIX!
-			const { name, selectedPriorities, selectedDates } = matrix;
+			const { name, selectedPriorities, dateOptions } = matrix;
 			setName(name);
 			setSelectedPriorities(selectedPriorities);
-			// setSelectedDates(selectedDates);
+			setDateOptions(dateOptions);
 		}
 	}, [matrix]);
 
@@ -130,14 +68,18 @@ const ModalEditMatrix: React.FC = () => {
 		return null;
 	}
 
-	const selectedDatesNames = Object.values(allDates)
-		.reduce((accumulator, current) => {
-			if (current.selected) {
-				accumulator.push(current.name);
-			}
-			return accumulator;
-		}, [])
-		.join(', ');
+	const selectedDatesNames =
+		dateOptions &&
+		Object.values(dateOptions)
+			.reduce((accumulator, current) => {
+				if (current.selected) {
+					accumulator.push(current.name);
+				}
+				return accumulator;
+			}, [])
+			.join(', ');
+
+	console.log(dateOptions);
 
 	return (
 		<Modal isOpen={isOpen} onClose={closeModal} positionClasses="!items-start mt-[150px]" customClasses="my-[2px]">
@@ -217,8 +159,8 @@ const ModalEditMatrix: React.FC = () => {
 										toggleRef={dropdownDatesRef}
 										isVisible={isDropdownDatesVisible}
 										setIsVisible={setIsDropdownDatesVisible}
-										allDates={allDates}
-										setAllDates={setAllDates}
+										dateOptions={dateOptions}
+										setDateOptions={setDateOptions}
 										customClasses="w-full ml-[0px]"
 										showSmartLists={true}
 									/>
@@ -234,7 +176,15 @@ const ModalEditMatrix: React.FC = () => {
 										label="All"
 										name="All"
 										checked={allPriorities}
-										onChange={() => setAllPriorities(true)}
+										onChange={() => {
+											setAllPriorities(true);
+
+											const selectedPrioritiesClone = { ...selectedPriorities };
+											Object.keys(selectedPriorities).forEach((key) => {
+												selectedPrioritiesClone[key] = false;
+											});
+											setSelectedPriorities(selectedPrioritiesClone);
+										}}
 										customOuterCircleClasses="!border-blue-500"
 										customInnerCircleClasses="!bg-blue-500"
 									/>
@@ -283,16 +233,18 @@ const ModalEditMatrix: React.FC = () => {
 							className="bg-blue-500 rounded py-1 cursor-pointer hover:bg-blue-600 min-w-[114px]"
 							onClick={async () => {
 								try {
-									// TODO: Save Matrix Settings somewhere in the backend.
-									await editMatrix({
+									// TODO: Add other properties (priorities and projects) to the payload.
+									const payload = {
 										matrixId: matrix._id,
 										payload: {
 											name,
-											// selectedDates,
+											dateOptions,
 											// selectedPriorities
 											// selectedProjects
 										},
-									});
+									};
+
+									await editMatrix(payload);
 									closeModal();
 								} catch (error) {
 									console.log(error);
@@ -357,170 +309,6 @@ const CustomCheckbox = ({ name, values, setValues, allPriorities, setAllPrioriti
 			<input type="checkbox" name={name} className="accent-blue-500" checked={isChecked} onChange={() => null} />
 			{name}
 		</div>
-	);
-};
-
-interface DropdownDatesProps extends DropdownProps {
-	selectedDate: string;
-	setSelectedDate: string;
-}
-
-const DropdownDates: React.FC<DropdownDatesProps> = ({ toggleRef, isVisible, setIsVisible, allDates, setAllDates }) => {
-	const [localAllDates, setLocalAllDates] = useState(allDates);
-
-	const [fromDate, setFromDate] = useState(null);
-	const [toDate, setToDate] = useState(null);
-	const [isDropdownFromDateVisible, setIsDropdownFromDateVisible] = useState(false);
-	const [isDropdownToDateVisible, setIsDropdownToDateVisible] = useState(false);
-
-	const dropdownFromDateRef = useRef();
-	const dropdownToDateRef = useRef();
-
-	console.log(fromDate);
-
-	const Duration = () => {
-		return (
-			<div className="flex-1 space-y-2 w-full">
-				<DurationOption
-					dropdownDateRef={dropdownFromDateRef}
-					isDropdownDateVisible={isDropdownFromDateVisible}
-					setIsDropdownDateVisible={setIsDropdownFromDateVisible}
-					date={fromDate}
-					setDate={setFromDate}
-					dropdownCustomClasses=" !ml-[100px] mt-[-100px]"
-				/>
-
-				<DurationOption
-					dropdownDateRef={dropdownToDateRef}
-					isDropdownDateVisible={isDropdownToDateVisible}
-					setIsDropdownDateVisible={setIsDropdownToDateVisible}
-					date={toDate}
-					setDate={setToDate}
-					dropdownCustomClasses=" !ml-[100px] mt-[-60px]"
-				/>
-			</div>
-		);
-	};
-
-	const DurationOption = ({
-		dropdownDateRef,
-		isDropdownDateVisible,
-		setIsDropdownDateVisible,
-		date,
-		setDate,
-		dropdownCustomClasses,
-	}) => (
-		<div className="flex-1 w-full" onClick={(e) => e.stopPropagation()}>
-			<div
-				ref={dropdownDateRef}
-				className="border border-color-gray-200 rounded p-1 px-2 flex justify-between items-center hover:border-blue-500 rounded-md cursor-pointer"
-				onClick={(e) => {
-					e.stopPropagation();
-					setIsDropdownDateVisible(!isDropdownDateVisible);
-				}}
-			>
-				<div className="max-w-[300px] truncate text-[12px]">
-					{date
-						? date.toLocaleString('en-US', {
-								year: 'numeric', // Full year
-								month: 'long', // Full month name
-								day: 'numeric', // Day of the month
-								hour: 'numeric', // Hour (in 12-hour AM/PM format)
-								minute: '2-digit', // Minute with leading zeros
-								hour12: true, // Use AM/PM
-							})
-						: 'No Date'}
-				</div>
-				<Icon
-					name="expand_more"
-					fill={0}
-					customClass={'text-color-gray-50 !text-[18px] hover:text-white cursor-pointer'}
-				/>
-			</div>
-
-			<DropdownCalendar
-				toggleRef={dropdownDateRef}
-				isVisible={isDropdownDateVisible}
-				setIsVisible={setIsDropdownDateVisible}
-				currDueDate={date}
-				setCurrDueDate={setDate}
-				customClasses={dropdownCustomClasses}
-			/>
-		</div>
-	);
-
-	return (
-		<Dropdown
-			toggleRef={toggleRef}
-			isVisible={isVisible}
-			setIsVisible={setIsVisible}
-			customClasses={'mt-[5px] shadow-2xl border border-color-gray-200 rounded-lg w-full'}
-		>
-			<div className="rounded" onClick={(e) => e.stopPropagation()}>
-				<div
-					className={classNames(
-						'p-1 gray-scrollbar max-h-[200px]',
-						!isDropdownFromDateVisible && !isDropdownToDateVisible ? 'overflow-auto' : 'overflow-hidden'
-					)}
-				>
-					{localAllDates &&
-						Object.entries(localAllDates).map(([key, value]) => {
-							const isDateSelected = value.selected;
-
-							return (
-								<div
-									key={value.name}
-									className="flex items-center justify-between gap-2 hover:bg-color-gray-300 p-2 rounded-lg cursor-pointer"
-									onClick={() => {
-										setLocalAllDates({
-											...localAllDates,
-											[key]: { ...value, selected: !localAllDates[key].selected },
-										});
-									}}
-								>
-									<div className="flex-1 flex items-center gap-2">
-										<Icon
-											name={value.iconName}
-											fill={0}
-											customClass={classNames(
-												'!text-[18px] hover:text-white cursor-pointer',
-												isDateSelected ? 'text-blue-500' : ''
-											)}
-										/>
-										<div className={isDateSelected ? 'text-blue-500' : ''}>{value.name}</div>
-										{key === 'duration' && <Duration />}
-									</div>
-									{isDateSelected && (
-										<Icon
-											name="check"
-											fill={0}
-											customClass={'text-blue-500 !text-[18px] hover:text-white cursor-pointer'}
-										/>
-									)}
-								</div>
-							);
-						})}
-				</div>
-
-				<div className="p-3 border-t border-color-gray-200 flex gap-2">
-					<button
-						className="flex-1 border border-color-gray-200 rounded py-1 cursor-pointer hover:bg-color-gray-200 min-w-[114px]"
-						onClick={() => setIsVisible(false)}
-					>
-						Cancel
-					</button>
-					<button
-						className="flex-1 bg-blue-500 rounded py-1 cursor-pointer hover:bg-blue-600 min-w-[114px]"
-						onClick={() => {
-							setAllDates(localAllDates);
-							setIsVisible(false);
-						}}
-					>
-						Ok
-					</button>
-				</div>
-			</div>
-		</Dropdown>
 	);
 };
 
