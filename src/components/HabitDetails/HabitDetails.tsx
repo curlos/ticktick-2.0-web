339375c6c4bee26242ab36037e32aaf92649ba71
Carlos Martinez <curlosmart@gmail.com>
2024-06-30 17:54:25 -0400

    Habit Details - Fix content names and flip between current and best streak

--- src/components/HabitDetails/HabitDetails.tsx
+++ src/components/HabitDetails/HabitDetails.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import Icon from '../Icon';
+import { useState } from 'react';
 
 // Should kind of be like TaskDetails but for Habits
 const HabitDetails = () => {
@@ -34,7 +35,17 @@ const TitleSection = () => (
 );
 
 const OverviewStatsSection = () => {
-	const OverviewStatCard = ({ name, iconName, iconColor = 'text-emerald-400', unitValue, unitType }) => {
+	const OverviewStatCard = ({
+		name,
+		iconName,
+		iconColor = 'text-emerald-400',
+		unitValue,
+		unitType,
+		isCurrentStreak = false,
+	}) => {
+		const [showBestStreak, setShowBestStreak] = useState(false);
+		const nameToShow = isCurrentStreak ? (showBestStreak ? 'Best Streak' : 'Current Streak') : name;
+
 		return (
 			<div className="bg-color-gray-600 rounded-md p-2">
 				<div className="flex items-center gap-1">
@@ -43,7 +54,17 @@ const OverviewStatsSection = () => {
 						fill={1}
 						customClass={classNames(iconColor, '!text-[18px] hover:text-white cursor-pointer')}
 					/>
-					<div>{name}</div>
+					<div className="flex items-center gap-1">
+						{nameToShow}{' '}
+						{isCurrentStreak && (
+							<Icon
+								name="sync"
+								fill={1}
+								customClass={'text-color-gray-50 !text-[18px] hover:text-white cursor-pointer'}
+								onClick={() => setShowBestStreak(!showBestStreak)}
+							/>
+						)}
+					</div>
 				</div>
 
 				<div>
@@ -64,18 +85,19 @@ const OverviewStatsSection = () => {
 				unitType="Day"
 			/>
 			<OverviewStatCard
-				name="Monthly check-ins"
+				name="Monthly Check-in Rate"
 				iconName="sword_rose"
 				iconColor="text-purple-400"
 				unitValue={1}
 				unitType="Day"
 			/>
 			<OverviewStatCard
-				name="Monthly check-ins"
+				name="Current Streak"
 				iconName="local_fire_department"
 				iconColor="text-orange-400"
 				unitValue={1}
 				unitType="Day"
+				isCurrentStreak={true}
 			/>
 		</div>
 	);
