c514676b869644f85ac13b508c0138f40cfc4007
Carlos Martinez <curlosmart@gmail.com>
2024-09-15 09:07:14 -0400

    Use real data for "RecentFocusedDurationCurve".

--- src/pages/StatsPage/OverviewSection/RecentFocusedDurationCurveCard.tsx
+++ src/pages/StatsPage/OverviewSection/RecentFocusedDurationCurveCard.tsx
@@ -1,42 +1,12 @@
 import { getFormattedDuration } from '../../../utils/helpers.utils';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
 import GeneralSelectButtonAndDropdown from '../GeneralSelectButtonAndDropdown';
-import { useState } from 'react';
-
-const data = [
-	{
-		name: 'July 8',
-		seconds: 15060,
-	},
-	{
-		name: 'July 9',
-		seconds: 19140,
-	},
-	{
-		name: 'July 10',
-		seconds: 20040,
-	},
-	{
-		name: 'July 11',
-		seconds: 20940,
-	},
-	{
-		name: 'July 12',
-		seconds: 18180,
-	},
-	{
-		name: 'July 13',
-		seconds: 21600,
-	},
-	{
-		name: 'July 14',
-		seconds: 10800,
-	},
-];
+import { useGetStatsForInterval } from '../hooks/useGetStatsForInterval';
 
 const RecentFocusedDurationCurveCard = () => {
-	const selectedOptions = ['Day', 'Week', 'Month'];
-	const [selected, setSelected] = useState(selectedOptions[0]);
+	const { selected, setSelected, selectedOptions, data } = useGetStatsForInterval('focusDuration');
+
+	console.log(data);
 
 	return (
 		<div className="bg-color-gray-600 p-3 rounded-lg flex flex-col h-[350px]">
@@ -64,12 +34,12 @@ const RecentFocusedDurationCurveCard = () => {
 					barSize={10}
 				>
 					<XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-					<YAxis dataKey="seconds" tickFormatter={(value) => `${getFormattedDuration(value, false)}`} />
+					<YAxis dataKey="score" tickFormatter={(value) => `${getFormattedDuration(value, false)}`} />
 					<Tooltip
 						content={({ payload }) => {
 							// "payload" property is an empty array if the tooltip is not active. Otherwise, if it is active, then it'll show an element in the "payload" array.
 							if (payload && payload[0]) {
-								const { name, seconds } = payload[0].payload;
+								const { name, score: seconds } = payload[0].payload;
 								return (
 									<div className="bg-black text-blue-500 p-2 rounded-md">{`${name}, ${getFormattedDuration(seconds, false)}`}</div>
 								);
@@ -80,7 +50,7 @@ const RecentFocusedDurationCurveCard = () => {
 					/>
 					<CartesianGrid strokeDasharray="3 3" opacity={0.2} />
 					<Bar
-						dataKey="seconds"
+						dataKey="score"
 						fill="#3b82f6"
 						background={{ fill: '#3a3a3a' }}
 						activeBar={{ fill: '#6ca6fc', cursor: 'pointer' }}
