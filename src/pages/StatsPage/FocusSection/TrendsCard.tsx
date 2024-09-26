12a07425ea0c9066a11202c7ba9d211d1ca61efc
Carlos Martinez <curlosmart@gmail.com>
2024-09-26 07:55:55 -0400

    BS

--- src/pages/StatsPage/FocusSection/TrendsCard.tsx
+++ src/pages/StatsPage/FocusSection/TrendsCard.tsx
@@ -31,7 +31,7 @@ const TrendsCard = () => {
 	const [endDate, setEndDate] = useState(new Date());
 
 	useEffect(() => {
-		// TODO: BS
+		// TODO: BS BS
 		if (selectedInterval === 'All' && focusRecords) {
 			const allFocusRecordDates = Object.keys(focusRecordsGroupedByDate).map((dateKey) => new Date(dateKey));
 			const newData = getUpdatedData(allFocusRecordDates);
